573822418a540ca8de48d88162286924edb09318
Munim Ahmed <munim.ah018@gmail.com>
2022-11-06 10:51:35 +0600

    Final Lab 1

---
+++ task1.asm
@@ -0,0 +1,28 @@
+.model small
+.stack 100h
+.code
+main proc
+    
+    
+mov cx, 255
+mov bx, 0
+
+top:
+    
+    mov ah,2
+    mov dx,bx
+    int 21h
+    inc bx
+    dec cx
+    
+    cmp cx,0
+    jnz top
+    jmp exit
+    
+    exit:
+        mov ah,4ch
+        int 21h
+        
+main endp
+end main
+
